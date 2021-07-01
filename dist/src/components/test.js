fb8f3ca88c39833da2e54f9104b6b6174f77f432
Nauman <nauman81007@gmail.com>
2021-07-01 23:28:35 +0500

    finalize

---
+++ dist/src/components/test.js
@@ -0,0 +1,25 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _jsxRuntime = require("react/jsx-runtime");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Test = function Test() {
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+    children: /*#__PURE__*/(0, _jsxRuntime.jsx)("h1", {
+      children: "Hellow World"
+    })
+  });
+};
+
+var _default = Test;
+exports.default = _default;
+
+//# sourceMappingURL=test.js.map
